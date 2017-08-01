8efe65c833532a34098aca20bbf5c3e5201e8456
Eloy <me@eloy.codes>
2017-08-01 21:41:10 +0200

    add Storybook: first story

---
+++ src/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => (
+  <div>
+    <h1>peluca</h1>
+    <p>This is a UI patterns library.</p>
+    <p>Initially I am building it for my personal website and other personal projects.</p>
+  </div>
+)
+
+export default About
---
+++ src/About/about.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import About from './About'
+
+import { storiesOf } from '@storybook/react'
+
+storiesOf('peluca', module)
+  .add('UI patterns library', () => <About />)
